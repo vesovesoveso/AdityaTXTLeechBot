2c223564a787be864464adf510cc906aedf2c546
adityasirji <146421388+adityasirji@users.noreply.github.com>
2023-10-01 23:25:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -401,7 +401,7 @@ async def account_login(bot: Client, m: Message):
             try:
                 Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
                 prog = await m.reply_text(Show)
-                cc = f"**Name :-** {name1} {res}🚦LPRPDV🚦.mkv\n**Batch :-** {raw_text0}\n**Index :-** {str(count).zfill(3)}\n\n**Download BY** :- {name1}'
+                cc = f'**Title »** {name1}🚦LPRPDV🚦.mkv\n**Batch »** {raw_text0}\n**Index »** {str(count).zfill(3)}\n\n**Download BY** :- {name1}'
                 cc1 = f"**Name :-** ** {name1} {res}.pdf\n**Batch :-** {raw_text0}\n**Index :-** {str(count).zfill(3)}'
                 #                         await prog.delete (True)
                 #                 if cmd == "pdf" or "drive" in url:
